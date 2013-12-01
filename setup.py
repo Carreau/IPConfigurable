8c80de44d823d35a4979d25c0a1c9df06e94f458
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-12-01 12:51:30 +0100

    clean

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='IPConfigurable',
     version='0.1dev',
-    packages=['ipconfigurable',],
+    packages=['IPConfigurable',],
     license='BSD',
     long_description=open('README.txt').read(),
 )
