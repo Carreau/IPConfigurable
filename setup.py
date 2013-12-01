d3b85ac0083fdb3abf2526c4e1efae95c87be6cf
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-12-01 12:58:11 +0100

    add lowercase

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='IPConfigurable',
     version='0.1dev',
-    packages=['IPConfigurable',],
+    packages=['IPConfigurable','ipconfigurable'],
     license='BSD',
     long_description=open('README.md').read(),
 )
